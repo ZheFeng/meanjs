a19897348d35b05fc47ee23bb23c63ac994c3994
Zhe Feng <zhe@jumiduo.com>
2015-03-24 15:51:38 +1100

    init meanjs

---
+++ app/models/message.server.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Message Schema
+ */
+var MessageSchema = new Schema({
+	text: {
+		type: String,
+		default: '',
+		required: 'Please fill Message text',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Message', MessageSchema);
